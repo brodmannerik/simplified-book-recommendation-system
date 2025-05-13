aacb284f38e3e93aa11e258d14ffa60013b4fc99
erik.brodmann <s86299@bht-berlin.de>
2025-05-14 00:38:24 +0200

    feat(deploy): first deploy #8

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { BrowserRouter, Route, Routes } from "react-router";
+import { HashRouter, Route, Routes } from "react-router";
 import App from "./App.tsx";
 import { AuthProvider } from "./context/AuthContext";
 import "./index.css";
@@ -12,7 +12,7 @@ import { store } from "./store/store.ts";
 createRoot(document.getElementById("root")!).render(
   <Provider store={store}>
     <AuthProvider>
-      <BrowserRouter>
+      <HashRouter>
         <Routes>
           <Route path="/" element={<App />}>
             <Route index element={<Login />} />
@@ -20,7 +20,7 @@ createRoot(document.getElementById("root")!).render(
             <Route path="book/:id" element={<BookDetails />} />
           </Route>
         </Routes>
-      </BrowserRouter>
+      </HashRouter>
     </AuthProvider>
   </Provider>
 );
